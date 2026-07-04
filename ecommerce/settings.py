45badc3b473060c3eaeee0c37f6d66c6fee6598a
matteo <matteo.sensi2@edu.unifi.it>
2026-07-05 01:29:13 +0200

    Update README.md to add spacing between product sections; change SECRET_KEY to use environment variable in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e3e@^t^_&!5^2%7r+!k+ay527b^8j^ub&hg^rx349+48ykgdb$'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
